381484332fd5eee26e16c6c51363a80a24247682
freek <freek@spatie.be>
2020-11-04 21:11:04 +0100

    wip

--- config/timber.php
+++ config/timber.php
@@ -2,12 +2,13 @@
 
 return [
     /*
-     * Regular logging calls can be sent to Timber as well.
+     * When enabled, all things logged to the application log
+     * will be sent to Timber as well.
      */
     'send_log_calls_to_timber' => true,
 
     /*
-     * The port number to communicate with Timber
+     * The port number to communicate with Timber.
      */
     'port' => 23517,
 ];
