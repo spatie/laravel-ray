a6d0139a41f3f99d61b0b30b3cdab2aa85bfc5c5
freek <freek@spatie.be>
2020-11-07 00:23:30 +0100

    wip

--- src/TimberServiceProvider.php
+++ src/TimberServiceProvider.php
@@ -32,7 +32,7 @@ class TimberServiceProvider extends ServiceProvider
 
     protected function registerBindings(): self
     {
-        $this->app->bind(Client::class, fn () => new Client('http://', config('timber.port')));
+        $this->app->bind(Client::class, fn () => new Client('http://localhost', config('timber.port')));
 
         $this->app->bind(Timber::class, function () {
             $client = app(Client::class);
