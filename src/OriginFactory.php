fb3e39ba905d9b61e8eae977573f4cc91172ffcd
freek <freek@spatie.be>
2020-12-17 09:40:09 +0100

    find location of dump calls

--- src/OriginFactory.php
+++ src/OriginFactory.php
@@ -6,8 +6,10 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Spatie\Backtrace\Backtrace;
 use Spatie\Backtrace\Frame;
+use Spatie\LaravelRay\DumpRecorder\DumpRecorder;
 use Spatie\Ray\Origin\Origin;
 use Spatie\Ray\Ray;
+use Symfony\Component\VarDumper\VarDumper;
 
 class OriginFactory
 {
@@ -42,6 +44,11 @@ class OriginFactory
             return $this->findFrameForQuery($frames);
         }
 
+
+        if ($frames[$indexOfRay] && $frames[$indexOfRay]->class === DumpRecorder::class) {
+            return $this->findFrameForDump($frames);
+        }
+
         return $frames[$indexOfRay + 1] ?? null;
     }
 
@@ -51,5 +58,19 @@ class OriginFactory
             ->search(fn(Frame $frame) => Str::startsWith($frame->class, 'Illuminate\Database'));
 
         return $frames[$indexOfLastDatabaseCall + 1] ?? null;
+    }
+
+    protected function findFrameForDump(Collection $frames): ?Frame
+    {
+        $indexOfDumpCall = $frames
+            ->search(function(Frame $frame) {
+                if (! is_null($frame->class)) {
+                    return false;
+                }
+
+                return in_array($frame->method, ['dump', 'dd']);
+            });
+
+        return $frames[$indexOfDumpCall + 1] ?? null;
     }
 }
