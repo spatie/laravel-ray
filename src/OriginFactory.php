973b3b990aa3edb4eb91e348726eec6d85cf9885
Bekzat Samatov <bekzatsmov@gmail.com>
2021-01-08 01:29:59 +0600

    array

--- src/OriginFactory.php
+++ src/OriginFactory.php
@@ -49,7 +49,7 @@ class OriginFactory
         /** @var Frame|null $foundFrame */
         $originFrame = $frames[$indexOfRay + 1] ?? null;
 
-        if ($originFrame && Str::endsWith($originFrame->file, join(DIRECTORY_SEPARATOR, array('ray', 'src', 'helpers.php')))) {
+        if ($originFrame && Str::endsWith($originFrame->file, join(DIRECTORY_SEPARATOR, ['ray', 'src', 'helpers.php']))) {
             $originFrame = $frames[$indexOfRay + 2] ?? null;
         }
 
@@ -73,7 +73,7 @@ class OriginFactory
             return $this->findFrameForEvent($frames);
         }
 
-        if (Str::endsWith($originFrame->file, join(DIRECTORY_SEPARATOR, array('vendor', 'psy', 'psysh', 'src', 'ExecutionLoopClosure.php')))) {
+        if (Str::endsWith($originFrame->file, join(DIRECTORY_SEPARATOR, ['vendor', 'psy', 'psysh', 'src', 'ExecutionLoopClosure.php']))) {
             $this->returnTinkerFrame();
         }
 
@@ -130,7 +130,7 @@ class OriginFactory
 
         /** @var Frame $foundFrame */
         if ($foundFrame = $frames[$indexOfEventDispatcherCall + 2]) {
-            if (Str::endsWith($foundFrame->file, join(DIRECTORY_SEPARATOR, array('Illuminate', 'Foundation', 'Events', 'Dispatchable.php')))) {
+            if (Str::endsWith($foundFrame->file, join(DIRECTORY_SEPARATOR, ['Illuminate', 'Foundation', 'Events', 'Dispatchable.php']))) {
                 $foundFrame = $frames[$indexOfEventDispatcherCall + 3];
             }
         };
@@ -149,7 +149,7 @@ class OriginFactory
                     $foundFrame = $frames[$indexOfLoggerCall + 3];
                 }
 
-                if (Str::endsWith($foundFrame->file, join(DIRECTORY_SEPARATOR, array('Illuminate', 'Foundation', 'helpers.php')))) {
+                if (Str::endsWith($foundFrame->file, join(DIRECTORY_SEPARATOR, ['Illuminate', 'Foundation', 'helpers.php']))) {
                     $foundFrame = $frames[$indexOfLoggerCall + 3];
                 }
             }
