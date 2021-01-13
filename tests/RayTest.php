3a104ebe56fa286781f5bf1c7a5e16627221c5ce
Patrick <trick.developer@gmail.com>
2021-01-13 17:02:20 -0500

    fix typo

--- tests/RayTest.php
+++ tests/RayTest.php
@@ -280,7 +280,7 @@ class RayTest extends TestCase
         // fix paths when running unit tests on windows platform (github actions)
         $json = json_encode($data);
         $json = str_replace('D:\\\\a\\\\laravel-ray\\\\laravel-ray', '', $json);
-        $json = str_replace('\\\\', '\\/', $json);
+        $json = str_replace('\\\\', '/', $json);
 
 
         $this->assertMatchesJsonSnapshot($json);
