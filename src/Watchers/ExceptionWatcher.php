93be86980d2561c50b33c9068fb9cebddd029991
freek <freek@spatie.be>
2021-01-29 15:35:41 +0100

    wip

--- src/Watchers/ExceptionWatcher.php
+++ src/Watchers/ExceptionWatcher.php
@@ -4,9 +4,12 @@ namespace Spatie\LaravelRay\Watchers;
 
 use Exception;
 use Facade\FlareClient\Flare;
+use Facade\FlareClient\Report;
+use Facade\FlareClient\Truncation\ReportTrimmer;
 use Illuminate\Log\Events\MessageLogged;
 use Illuminate\Support\Facades\Event;
 use Spatie\LaravelRay\Ray;
+use Throwable;
 
 class ExceptionWatcher extends Watcher
 {
@@ -25,17 +28,16 @@ class ExceptionWatcher extends Watcher
 
             $exception = $message->context['exception'];
 
+            $meta = [];
+
+            if ($flareReport = $this->getFlareReport($exception)) {
+                $meta['flare_report'] = $flareReport;
+            }
+
             /** @var Ray $ray */
             $ray = app(Ray::class);
 
-
-            /** @var Flare $flare */
-            $flare = app(Flare::class);
-
-            /** @var \Facade\FlareClient\Report $report */
-            $report = $flare->createReport($exception);
-
-            $ray->exception($exception, ['flare_report' => $report->toArray()]);
+            $ray->exception($exception, $meta);
         });
     }
 
@@ -50,5 +52,19 @@ class ExceptionWatcher extends Watcher
         }
 
         return true;
+    }
+
+    public function getFlareReport(Throwable $exception): ?array
+    {
+        if (! class_exists(Flare::class)) {
+            return null;
+        }
+
+        /** @var \Facade\FlareClient\Flare $flare */
+        $flare = app(Flare::class);
+
+        $report = $flare->createReport($exception);
+
+        return (new ReportTrimmer())->trim($report->toArray());
     }
 }
