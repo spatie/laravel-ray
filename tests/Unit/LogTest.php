33f37e0486198cbf4cfd3a5780ef5965d620dc25
Patrick <trick.developer@gmail.com>
2021-02-05 09:20:27 -0500

    remove exception-related changes

--- tests/Unit/LogTest.php
+++ tests/Unit/LogTest.php
@@ -4,7 +4,6 @@ namespace Spatie\LaravelRay\Tests\Unit;
 
 use Illuminate\Support\Facades\Log;
 use Spatie\LaravelRay\Tests\TestCase;
-use Spatie\LaravelRay\Watchers\ExceptionWatcher;
 use Spatie\Ray\Settings\Settings;
 
 class LogTest extends TestCase
@@ -25,28 +24,5 @@ class LogTest extends TestCase
         Log::info('hey');
 
         $this->assertCount(0, $this->client->sentPayloads());
-    }
-
-    /** @test */
-    public function it_will_send_logs_with_exceptions_to_ray_when_enabled()
-    {
-        app(Settings::class)->send_log_calls_to_ray = true;
-        app(ExceptionWatcher::class)->enable();
-
-        // counters increase by two each time because each Log::error() call
-        // also sends a color('red') payload
-        $contexts = [
-            3 => ['exception' => new \Exception('test')],
-            5 => ['exception' => new \stdClass()],
-            7 => [],
-        ];
-
-        foreach ($contexts as $counter => $context) {
-            Log::error('hey', $context);
-
-            $this->assertCount($counter, $this->client->sentPayloads());
-        }
-
-        $this->assertMatchesOsSafeSnapshot($this->client->sentPayloads());
     }
 }
