ba713ecb6e96c375a6fa9dd619061c0f21323ac2
Mior Muhammad Zaki <crynobone@gmail.com>
2024-01-15 18:50:17 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Unit/CacheTest.php
+++ tests/Unit/CacheTest.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Cache;
 
+beforeEach(function () {
+    config('cache.default', 'array');
+});
+
 it('can detect when something gets cached', function () {
     ray()->showCache();
 
