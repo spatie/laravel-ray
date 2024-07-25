f2ae7d0e36e82a1dcafdc4850c90e4aab4ac74d1
Patrick O'Meara <pat@maintainly.com>
2024-07-25 20:47:39 +1000

    Add ConditionalQueryWatcher
    This piggybacks of the QueryWatcher but only sends queries to ray that satify a conditional callback
    
    I've added update here as a helper method.

--- tests/Unit/QueryTest.php
+++ tests/Unit/QueryTest.php
@@ -104,3 +104,45 @@ it('an eloquent query can be sent to ray', function () {
 
     expect($user)->toBeInstanceOf(User::class);
 });
+
+it('can show only update queries', function () {
+    ray()->showUpdateQueries();
+
+    $user = User::query()->create(['email' => 'john@example.com']);
+    $user->update(['email' => 'joan@example.com']);
+    $user = User::query()->find($user->id);
+    $user->delete();
+
+    expect($this->client->sentPayloads())->toHaveCount(1);
+
+    $payload = $this->client->sentPayloads();
+
+    $this->assertStringStartsWith('update', Arr::get($payload, '0.content.sql'));
+});
+
+it('can show only update queries and return the results', function () {
+    $user = ray()->showUpdateQueries(function (): User {
+        $user = User::query()->create(['email' => 'john@example.com']);
+        $user->update(['email' => 'joan@example.com']);
+
+        return $user;
+    });
+
+    expect($this->client->sentRequests())->toHaveCount(1);
+    expect($user)->toBeInstanceOf(User::class);
+
+    $this->assertSame('joan@example.com', $user->email);
+});
+
+it('can stop showing update queries', function () {
+    $user = User::query()
+        ->create(['email' => 'john@example.com']);
+
+    ray()->showUpdateQueries();
+    $user->update(['email' => 'joan@example.com']);
+    ray()->stopShowingUpdateQueries();
+    $user->update(['email' => 'joe@example.com']);
+
+    expect($this->client->sentRequests())->toHaveCount(1);
+});
+
