7ba66769b8a00a9ce155fb3381673d508f95899c
Patrick O'Meara <pat@maintainly.com>
2024-07-26 11:56:05 +1000

    Don't use __construct

--- src/Watchers/DeleteQueryWatcher.php
+++ src/Watchers/DeleteQueryWatcher.php
@@ -6,17 +6,14 @@ use Spatie\Ray\Settings\Settings;
 
 class DeleteQueryWatcher extends ConditionalQueryWatcher
 {
-    public function __construct()
-    {
-        parent::__construct(function (string $query) {
-            return str_starts_with(strtolower($query), 'delete');
-        });
-    }
-
     public function register(): void
     {
         $settings = app(Settings::class);
 
         $this->enabled = $settings->send_delete_queries_to_ray ?? false;
+
+        $this->setConditionalCallback(function (string $query) {
+            return str_starts_with(strtolower($query), 'delete');
+        });
     }
 }
--- src/Watchers/SelectQueryWatcher.php
+++ src/Watchers/SelectQueryWatcher.php
@@ -6,17 +6,14 @@ use Spatie\Ray\Settings\Settings;
 
 class SelectQueryWatcher extends ConditionalQueryWatcher
 {
-    public function __construct()
-    {
-        parent::__construct(function (string $query) {
-            return str_starts_with(strtolower($query), 'select');
-        });
-    }
-
     public function register(): void
     {
         $settings = app(Settings::class);
 
         $this->enabled = $settings->send_select_queries_to_ray ?? false;
+
+        $this->setConditionalCallback(function (string $query) {
+            return str_starts_with(strtolower($query), 'select');
+        });
     }
 }
